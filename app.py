a402efed14ef36199406097f5ed971ffcae8a251
silvermete0r <108217670+silvermete0r@users.noreply.github.com>
2023-12-02 00:00:39 +0600

    initial-commit

---
+++ app.py
@@ -0,0 +1,116 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+from projects import day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25, day26, day27, day28, day29, day30, day31
+
+# Set Page Config
+st.set_page_config(
+    page_title = 'DataFlow App',
+    page_icon = '📊',
+    layout = 'wide',
+    initial_sidebar_state = 'auto',
+)
+
+# Home Page Content
+def main():
+    st.title('31 Days DS/ML Challenge')
+    st.write('''
+            [![Star](https://img.shields.io/github/stars/silvermete0r/31github.svg?logo=github&style=flat-square)](https://github.com/silvermete0r/31github)&nbsp;
+            [![Fork](https://img.shields.io/github/forks/silvermete0r/31github.svg?logo=github&style=flat-square)](https://github.com/silvermete0r/31github)&nbsp;
+            [![Watch](https://img.shields.io/github/watchers/silvermete0r/31github.svg?logo=github&style=flat-square)](https://github.com/silvermete0r/31github)&nbsp;
+            [![Follow](https://img.shields.io/github/followers/silvermete0r.svg?logo=github&style=flat-square)](https://github.com/silvermete0r/31github)&nbsp;
+            [![License](https://img.shields.io/github/license/silvermete0r/31github.svg?logo=github&style=flat-square)](https://github.com/silvermete0r/31github)
+    ''')
+    st.write('👋 My name is Arman, and I\'m very interested in Data Science and Machine Learning!')
+
+# Multipage WebApp Design
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+    
+    def add_app(self, title, function):
+        self.apps.append({
+            'title': title,
+            'function': function
+        })
+    
+    def run():
+        with st.sidebar:
+            # Set Sidebar Content
+            st.sidebar.image('media/logo.png', use_column_width=True)
+            app = option_menu(
+                menu_title = None,
+                options = ['Main', 'Day #1', 'Day #2', 'Day #3', 'Day #4', 'Day #5', 'Day #6', 'Day #7', 'Day #8', 'Day #9', 'Day #10', 'Day #11', 'Day #12', 'Day #13', 'Day #14', 'Day #15', 'Day #16', 'Day #17', 'Day #18', 'Day #19', 'Day #20', 'Day #21', 'Day #22', 'Day #23', 'Day #24', 'Day #25', 'Day #26', 'Day #27', 'Day #28', 'Day #29', 'Day #30', 'Day #31'],
+                icons = ['menu-up'],
+                menu_icon = 'chat-text-fill',
+                default_index = 0
+            )
+            st.info('31 Days Educational Challenge for Data Science / Machine Learning for the program [GrowthHungry](https://www.growthhungry.life/challenge) Challenge.')
+            st.caption('Made with ❤️ by [DataFlow](https://dataflow.kz) team.')
+
+        # Set Main Content
+        if app == 'Main':
+            main()
+        if app == 'Day #1':
+            day1.app()
+        if app == 'Day #2':
+            day2.app()
+        if app == 'Day #3':
+            day3.app()
+        if app == 'Day #4':
+            day4.app()
+        if app == 'Day #5':
+            day5.app()
+        if app == 'Day #6':
+            day6.app()
+        if app == 'Day #7':
+            day7.app()
+        if app == 'Day #8':
+            day8.app()
+        if app == 'Day #9':
+            day9.app()
+        if app == 'Day #10':
+            day10.app()
+        if app == 'Day #11':
+            day11.app()
+        if app == 'Day #12':
+            day12.app()
+        if app == 'Day #13':
+            day13.app()
+        if app == 'Day #14':
+            day14.app()
+        if app == 'Day #15':
+            day15.app()
+        if app == 'Day #16':
+            day16.app()
+        if app == 'Day #17':
+            day17.app()
+        if app == 'Day #18':
+            day18.app()
+        if app == 'Day #19':
+            day19.app()
+        if app == 'Day #20':
+            day20.app()
+        if app == 'Day #21':
+            day21.app()
+        if app == 'Day #22':
+            day22.app()
+        if app == 'Day #23':
+            day23.app()
+        if app == 'Day #24':
+            day24.app()
+        if app == 'Day #25':
+            day25.app()
+        if app == 'Day #26':
+            day26.app()
+        if app == 'Day #27':
+            day27.app()
+        if app == 'Day #28':
+            day28.app()
+        if app == 'Day #29':
+            day29.app()
+        if app == 'Day #30':
+            day30.app()
+        if app == 'Day #31':
+            day31.app()
+    run()
---
+++ projects/day1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title('Day #1')
+    st.subheader('Linear Regression Problem')
